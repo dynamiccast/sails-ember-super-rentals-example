7cb1bb923c9e792cc6789a58ea340f79bd64f449
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-24 20:35:21 +0200

    Add rental model

---
+++ backend/api/controllers/RentalController.js
@@ -0,0 +1,11 @@
+/**
+ * RentalController
+ *
+ * @description :: Server-side logic for managing rentals
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
